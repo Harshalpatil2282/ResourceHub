e70ffa80b8104eb42fee07425932154859ef25c0
HarshalPatil <patilharshal2282@gmail.com>
2025-06-28 20:04:28 +0530

    Resolve error for creating program.. perfectly creat program.

--- server/controllers/programController.js
+++ server/controllers/programController.js
@@ -3,10 +3,17 @@ const Program = require('../models/Program');
 exports.createProgram = async (req, res) => {
   try {
     const { name, universityId } = req.body;
-    const program = new Program({ name, universityId });
+
+    if (!name || !universityId) {
+      return res.status(400).json({ msg: 'Name and universityId are required' });
+    }
+
+    const program = new Program({ name, university: universityId });
     await program.save();
+
     res.status(201).json(program);
   } catch (err) {
+    console.error(err);
     res.status(500).json({ msg: 'Error creating program', error: err.message });
   }
 };
@@ -14,9 +21,9 @@ exports.createProgram = async (req, res) => {
 exports.getProgramsByUniversity = async (req, res) => {
   try {
     const { universityId } = req.params;
-    const programs = await Program.find({ universityId });
+    const programs = await Program.find({ university: universityId });
     res.json(programs);
   } catch (err) {
-    res.status(500).json({ msg: 'Error fetching programs' });
+    res.status(500).json({ msg: 'Error fetching programs', error: err.message });
   }
 };
--- server/routes/programRoutes.js
+++ server/routes/programRoutes.js
@@ -1,25 +1,11 @@
-// const express = require('express');
-// const { createProgram, getProgramsByUniversity } = require('../controllers/programController');
-// const { verifyToken, checkRole } = require('../middleware/auth');
-
-// const router = express.Router();
-
-// router.post('/', verifyToken, checkRole(['admin']), createProgram);
-// router.get('/:universityId', verifyToken,checkRole(['admin','user']), getProgramsByUniversity);
-
-// module.exports = router;
 const express = require('express');
 const router = express.Router();
-const Program = require('../models/Program');
+const { createProgram, getProgramsByUniversity } = require('../controllers/programController');
 
-// GET all programs
-router.get('/', async (req, res) => {
-  try {
-    const programs = await Program.find();
-    res.json(programs);
-  } catch (err) {
-    res.status(500).json({ msg: 'Failed to get programs' });
-  }
-});
+// POST to create a program
+router.post('/', createProgram);
+
+// GET programs by university ID
+router.get('/university/:universityId', getProgramsByUniversity);
 
 module.exports = router;
