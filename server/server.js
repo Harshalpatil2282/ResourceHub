a2e9b0376cc49dc1de694af808e37ae5e14e1d3b
HarshalPatil <patilharshal2282@gmail.com>
2025-07-10 20:23:39 +0530

    serving frontend from backend

--- server/server.js
+++ server/server.js
@@ -3,12 +3,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-
+const path = require('path');
 dotenv.config();
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const _dirname = path.resolve();
 
 // Import routes
 const authRoutes = require('./routes/authRoutes');
@@ -29,11 +32,15 @@ app.use('/api/folders', folderRoutes);
 app.use('/api/files', fileRoutes);
 app.use('/api/activities' , activityRoutes);
 
+app.use(express.static(path.join(_dirname, "..","client/build")));
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(_dirname, "..","client","build", "index.html"));
+})
+
+
 // DB Connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
     console.log('MongoDB connected');
     app.listen(process.env.PORT, () => {
         console.log(`Server running on port ${process.env.PORT}`);
